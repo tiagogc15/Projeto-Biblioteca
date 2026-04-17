b7aeeba4a19c882ffff5572821b0aeea79a12002
tiagogc15 <tiago.carvalho015@hotmail.com>
2026-04-16 23:52:37 -0300

    fix database definitivo real

--- biblioteca/biblioteca/settings.py
+++ biblioteca/biblioteca/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'biblioteca.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-        'default': dj_database_url.config(
+    'default': dj_database_url.config(
         default='sqlite:///db.sqlite3',
         conn_max_age=600
     )
