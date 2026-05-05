b25e14ddc60bcbd885a8eb49e2252f520da2bab6
tiagogc15 <tiago.carvalho015@hotmail.com>
2026-05-05 13:44:00 -0300

    fix: usar view correta da home

--- biblioteca/biblioteca/urls.py
+++ biblioteca/biblioteca/urls.py
@@ -18,12 +18,10 @@ from django.contrib import admin
 from django.urls import path, include
 from django.shortcuts import render
 
-def home(request):
-    return render(request, 'home.html')
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home),
     path('', include('core.urls')),
 ]
 
